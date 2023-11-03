bba491c0043af6050d3504b9bdb2d7e66138d3b2
jitaeyu <yjt3602@gmail.com>
2023-11-03 21:53:23 +0900

    아스키코드 변환 완료

--- src/main/java/com/secure/cryptography/controller/MainContorller.java
+++ src/main/java/com/secure/cryptography/controller/MainContorller.java
@@ -1,7 +1,14 @@
 package com.secure.cryptography.controller;
 
+import com.secure.cryptography.domain.Entity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class MainContorller {
@@ -9,5 +16,54 @@ public class MainContorller {
     @GetMapping("/")
     public String index(){
         return "page/index";
+    }
+
+//    @PostMapping("/read")
+//    public void encryptionText(HttpServletRequest request) throws UnsupportedEncodingException {
+////        request.setCharacterEncoding("utf-8");
+////        String ttest = request.getParameter("text");
+//        System.out.println("들어옴 read");
+////    return "";
+//    }
+
+
+    @ResponseBody
+    @RequestMapping(value = "/read", method = { RequestMethod.POST })
+    public void test(@RequestBody Object dto) throws Exception {
+//        int i =0;
+        String jsonString = dto.toString();
+//        System.out.println("dto"+dto);
+//        System.out.println("스트링"+jsonString);
+//        int len = dto.length();
+        String[] newline = jsonString.split("\\n");
+//        System.out.println("newline"+newline);
+        String[] asc;
+        List<Integer> list = new ArrayList();
+        for (int i = 0 ; i<newline.length; i++){
+            System.out.println(i+"i");
+            System.out.println("값"+newline[i]+"\n");
+            for(int k =0; k<newline[i].length();k++){
+                char character = newline[i].charAt(k); // 문자열에서 한 글자 가져오기
+                int ascii = (int) character; // 글자를 ASCII 코드로 변환
+                System.out.println("문자: " + character + ", ASCII 코드+1암호화: " + ((int)ascii+1));
+//                asc=newline[i].split("");
+//                System.out.println("start"+asc+"아스키:");
+                list.add(ascii);
+            }
+        }
+        List<Character> bs = new ArrayList<>();
+        for(int k:list) {//K에 a에 있는것을 모두 넘겨라 (확인하고 넘기는거니까 하나씩보기가능)
+            System.out.println("복호시작: "+(char)k);
+            bs.add((char)k);
+
+        }
+
+
+//        System.out.println(len+"개수");
+//        while (len<i){
+//
+//        }
+//        System.out.println(dto);
+        System.out.println("들어옴 read");
     }
 }
