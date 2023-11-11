1814eb42841ddcd454ca941cbd5f559fe316c56f
jitaeyu <yjt3602@gmail.com>
2023-11-11 21:31:17 +0900

    암호화 파일생성 완료

--- src/main/java/com/secure/cryptography/controller/MainContorller.java
+++ src/main/java/com/secure/cryptography/controller/MainContorller.java
@@ -1,20 +1,19 @@
 package com.secure.cryptography.controller;
 
-import com.secure.cryptography.domain.Entity;
+import com.secure.cryptography.file.FileDownload;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class MainContorller {
 
+    FileDownload fileDownload = new FileDownload();
+
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "page/index";
     }
 
@@ -28,7 +27,7 @@ public class MainContorller {
 
 
     @ResponseBody
-    @RequestMapping(value = "/read", method = { RequestMethod.POST })
+    @RequestMapping(value = "/read", method = {RequestMethod.POST})
     public List<Integer> test(@RequestBody Object dto) throws Exception {
 //        int i =0;
         String jsonString = dto.toString();
@@ -39,32 +38,35 @@ public class MainContorller {
 //        System.out.println("newline"+newline);
         String[] asc;
         List<Integer> list = new ArrayList();
-        for (int i = 0 ; i<newline.length; i++){
-            System.out.println(i+"i");
-            System.out.println("값"+newline[i]+"\n");
-            for(int k =0; k<newline[i].length();k++){
+        for (int i = 0; i < newline.length; i++) {
+            System.out.println(i + "i");
+            System.out.println("값" + newline[i] + "\n");
+            for (int k = 0; k < newline[i].length(); k++) {
                 char character = newline[i].charAt(k); // 문자열에서 한 글자 가져오기
                 int ascii = (int) character; // 글자를 ASCII 코드로 변환
-                System.out.println("문자: " + character + ", ASCII 코드+1암호화: " + ((int)ascii+1));
+                System.out.println("문자: " + character + ", ASCII 코드+1암호화: " + ((int) ascii + 1));
 //                asc=newline[i].split("");
 //                System.out.println("start"+asc+"아스키:");
-                list.add((int)ascii+1);
+                list.add((int) ascii + 1);
 
             }
         }
         List<Character> bs = new ArrayList<>();
-        for(int k:list) {//K에 a에 있는것을 모두 넘겨라 (확인하고 넘기는거니까 하나씩보기가능)
-            System.out.println("복호시작: "+(char)k);
-            bs.add((char)k);
+        for (int k : list) {//K에 a에 있는것을 모두 넘겨라 (확인하고 넘기는거니까 하나씩보기가능)
+            System.out.println("복호시작: " + (char) k);
+            bs.add((char) k);
 
         }
-
+        String path = "C:\\cryptography";
+        String fname = "encryption.txt";
+        fileDownload.write(path,fname,list);
 
 //        System.out.println(len+"개수");
 //        while (len<i){
 //
 //        }
 //        System.out.println(dto);
+
         System.out.println("들어옴 read");
         return list;
     }
---
+++ src/main/java/com/secure/cryptography/file/FileDownload.java
@@ -0,0 +1,20 @@
+package com.secure.cryptography.file;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class FileDownload {
+    public void write(String path, String fileName, List<Integer> content){
+        File file = new File(path,fileName);
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+            writer.write(String.valueOf(content));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
